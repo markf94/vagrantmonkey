517df4280d2900dda7a729eb2f88f60a5578eaca
morrieinmaas <m.schlichting@student.maastrichtuniversity.nl>
2016-02-20 00:04:54 +0100

    all added

---
+++ www/monkeytestmoritz/logs/jcontroller.log.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2016-02-18 13:32:39 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2016-02-18T13:32:39+00:00	INFO 127.0.0.1	controller	Holding edit ID com_gantry.edit.template.10 Array (     [0] => 10 ) 
